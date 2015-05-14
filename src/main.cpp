7501680ecd3a9601a9eb1a6a72b35479801a9ae8
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-05-13 23:30:11 -0400

    Added namespace and header files to main
    Added the a namespace GCompiler for the AST and visitor classes.
    Hence a using GCompiler needed to be added to the main file. All phases
    of the compiler except for the parser and lexer will be under the GCompiler
    namespace. The parser and lexer could not be added since they generate C code.
    Eventually the flex and bison grammar would be modified to generate C++ code
    allowing us to put the first two phases in the same namespace.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include<goParser.hpp>
 #include<goLexer.hpp>
 #include<iostream>
+#include<node-visitor.hpp>
+#include<ast.hpp>
+using namespace GCompiler;
 int main(int argc, char* argv[]) {
 		yyin = fopen(argv[1], "r");	
 		int res = yyparse();
 		exit(res);
 }
-
-

7212147e4a98d39925bde8c122f7d40e3a54cfda
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-05-14 23:42:31 -0400

    Removed class template for NodeVisitor
    Since the idea of polymorphic visitor pattern was dropped, all
    methods of the nodeVisitor class will return void instead of a
    template type. Hence the tempate for the NodeVisitor was removed.
    
    @author Sameer Jagdale

--- include/node-visitor.hpp
+++ include/node-visitor.hpp
@@ -1,9 +1,8 @@
 #ifndef __NODE_VISITOR__
 #define __NODE_VISITOR__
-template<typename T> 
 class NodeVisitor {
 	public:
-	virtual T caseAstNode();
-    virtual T caseProgram();
+	virtual void caseAstNode();
+    virtual void caseProgram();
 };
 #endif
